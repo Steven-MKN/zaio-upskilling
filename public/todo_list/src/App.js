f0cf0f4f2c447a621be4dd40ec69d00e1079da74
Steven-MKN <steventmokoena@gmail.com>
2020-12-31 09:54:31 +0200

    completed todo_list app

--- public/todo_list/src/App.js
+++ public/todo_list/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Todos from './Todos'
+import AddTodo from './AddTodo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 0, complete: true, content: 'learn react'},
+      {id: 1, complete: false, content: 'play csgo'}
+    ]
+  }
+
+  addTodo = (todo) => {
+    if (this.state.todos && this.state.todos.length > 0)
+      todo.id = Number.parseInt(this.state.todos[this.state.todos.length - 1].id) + 1
+    else todo.id = 1
+
+    let newTodos = this.state.todos
+    newTodos.push(todo)
+
+    this.setState({
+      todos: newTodos
+    })
+
+    return
+  }
+
+  deleteTodo = (id) => {
+    let newTodos = this.state.todos.filter(t => {
+      return t.id !== id
+    })
+
+    this.setState({
+      todos: newTodos
+    })
+  }
+
+  completeTodo = (id) => {
+    let newTodos = [ ...this.state.todos ]
+
+    newTodos.forEach(todo => {
+      if (todo.id === id){
+        todo.complete = true
+        return
+      }
+    });
+
+    this.setState({
+      todos: newTodos
+    })
+  }
+
+  undoTodo = (id) => {
+    let newTodos = [ ...this.state.todos ]
+
+    newTodos.forEach(todo => {
+      if (todo.id === id){
+        todo.complete = false
+        return
+      }
+    });
+
+    this.setState({
+      todos: newTodos
+    })
+  }
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's'</h1>
+        <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } completeTodo={ this.completeTodo } undoTodo={ this.undoTodo }/>
+        <AddTodo addTodo={ this.addTodo } />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ public/todo_list/src/Todos.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const todos = ({ todos, deleteTodo, addTodo, completeTodo, undoTodo }) => {
+    let todoList = []
+    let completeList = [] 
+    
+    todos.forEach( todo => {
+        if (todo.complete){
+            completeList.push(
+                <div className="collection-item" key={ todo.id }>
+                    <a className="waves-effect waves-light btn-small my-list-btn grey" onClick={ () => undoTodo(todo.id) }>
+                        <i className="material-icons">undo</i>
+                    </a>
+                    <a className="waves-effect waves-light btn-small my-list-btn grey" onClick={ () => deleteTodo(todo.id) }>
+                        <i className="material-icons">delete</i>
+                    </a>
+                    <span className="todo-conetnt">
+                        <del>{ todo.content }</del>
+                    </span>
+                </div>
+            )
+        } else {
+            todoList.push(
+                <div className="collection-item" key={ todo.id }>
+                    <a className="waves-effect waves-light btn-small my-list-btn" onClick={ () => completeTodo(todo.id) }>
+                        <i className="material-icons">check</i>
+                    </a>
+                    <span className="todo-conetnt">{ todo.content }</span>
+                </div>
+            )
+        }
+    })
+
+    return(
+        <div className="container">
+            { 
+                todoList.length > 0 ? (
+                    <div className="collection todos">
+                        { todoList }
+                    </div>
+                ) : (
+                    <div className="collection todos">
+                    <p className="center">You have no todo's left</p>
+                    </div>
+                    ) 
+            }
+            <div className="collection todos">
+                { completeList }
+            </div>
+        </div>
+    )
+}
+
+export default todos
